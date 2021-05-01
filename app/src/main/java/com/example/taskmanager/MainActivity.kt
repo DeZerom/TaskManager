362716501973ffb474101705fd0768f48e7407cc
DeZerom <47084837+DeZerom@users.noreply.github.com>
2021-05-01 14:20:30 +0400

    It crushes
    Toolbar's done. Started work on navigation, but it crushes

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.taskmanager
 
+import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tb = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(tb)
+
+        tb.title = getString(R.string.app_name)
     }
 }
