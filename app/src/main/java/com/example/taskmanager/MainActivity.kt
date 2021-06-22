5edd0e37f0f266120df3aaf68f4d44249da54a8c
DeZerom <47084837+DeZerom@users.noreply.github.com>
2021-06-23 00:06:47 +0400

    Crushes fixed. Home fragment added

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.taskmanager
 
-import android.app.Application
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,5 +22,18 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(tb)
 
         tb.title = getString(R.string.app_name)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.main_drawerLayout)
+        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navContr = findNavController(R.id.nav_host_fragment)
+        appBarConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.settingsFragment,
+            ), drawerLayout)
+        setupActionBarWithNavController(navContr, appBarConfiguration)
+        navView.setupWithNavController(navContr)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
