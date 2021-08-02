36867f075634ece4d130bc2f6ac782cfbe6079ca
DeZerom <47084837+DeZerom@users.noreply.github.com>
2021-08-02 22:40:40 +0400

    Changing projectOwnerId in AddTask
    It's now possible to change
    Task.projectOwnerId in AddTask
    fragment. Spinner + logic were added in
    AddTaskFragmnet

--- app/src/main/java/com/example/taskmanager/fragments/task/add/AddTaskFragment.kt
+++ app/src/main/java/com/example/taskmanager/fragments/task/add/AddTaskFragment.kt
@@ -5,18 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.taskmanager.R
 import com.example.taskmanager.data.project.Project
 import com.example.taskmanager.data.task.Task
+import com.example.taskmanager.viewmodels.ProjectViewModel
 import com.example.taskmanager.viewmodels.TaskViewModel
 import kotlinx.android.synthetic.main.fragment_add_task.view.*
 
 class AddTaskFragment : Fragment() {
-    private lateinit var mTaskModel: TaskViewModel
+    /**
+     * Contains default [Project] that is considered to be parent of a new task
+     */
     private lateinit var mParentProject: Project
+
+    /**
+     * Adapter for spinner view
+     * @see addTask_spinner
+     */
+    private lateinit var mSpinnerAdapter: ArrayAdapter<Project>
+    private lateinit var mTaskModel: TaskViewModel
+    private lateinit var mProjectViewModel: ProjectViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,17 +37,35 @@ class AddTaskFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add_task, container, false)
 
-        //task view model
+        //view models
         mTaskModel = ViewModelProvider(this).get(TaskViewModel::class.java)
+        mProjectViewModel = ViewModelProvider(this).get((ProjectViewModel::class.java))
 
         //get parent project
         val tmp = arguments?.let { AddTaskFragmentArgs.fromBundle(it).item }
         tmp?.let { mParentProject = it }
 
+        //spinner
+        val spinner = view.addTask_spinner
+        mSpinnerAdapter = ArrayAdapter<Project>(requireContext(),
+            R.layout.support_simple_spinner_dropdown_item)
+        spinner.adapter = mSpinnerAdapter
+        //set data to spinner
+        mProjectViewModel.allProjects.observe(viewLifecycleOwner) {
+            mSpinnerAdapter.clear()
+            mSpinnerAdapter.addAll(it)
+            //set default selection
+            spinner.setSelection(mSpinnerAdapter.getPosition(it.find { p ->
+                return@find p.id == mParentProject.id
+            }))
+        }
+
+        //add button
         val edit = view.addTaskFragment_editText
         val btn = view.addTaskFragment_button
         btn.setOnClickListener {
-            val task = getTask(edit.text.toString())
+            val projectOwnerId = (spinner.selectedItem as Project).id
+            val task = createTask(edit.text.toString(), projectOwnerId)
             task?.let { mTaskModel.addTask(it) }
             //navigate back
             //TODO may cause problems
@@ -45,16 +75,28 @@ class AddTaskFragment : Fragment() {
         return view
     }
 
-    private fun getTask(name: String): Task? {
+    /**
+     * Creates a new [Task] with given [Task.name] and [Task.projectOwnerId]. If name is incorrect,
+     * it will return Null.
+     * @return [Task] if name is correct. Null otherwise.
+     * @see checkName
+     */
+    private fun createTask(name: String, parentProjectId: Int): Task? {
         if (!checkName(name)) return null
 
-        return Task(0, name, mParentProject.id)
+        return Task(0, name, parentProjectId)
     }
 
+    /**
+     * Checks if name is blank. Shows [Toast] if name is blank.
+     * @return True if name is blank, false if name is not blank
+     * @see isBlank
+     * @see isNotBlank
+     */
     private fun checkName(name: String): Boolean {
-        Toast.makeText(requireContext(), R.string.addTaskFragment_toast_emptyName,
-            Toast.LENGTH_SHORT).show()
+        if (name.isBlank()) Toast.makeText(requireContext(),
+            R.string.addTaskFragment_toast_emptyName, Toast.LENGTH_SHORT).show()
 
-        return name.isNotEmpty()
+        return name.isNotBlank()
     }
 }
