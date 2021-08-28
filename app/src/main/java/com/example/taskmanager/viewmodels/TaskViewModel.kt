b56000f158ceed11a02c8bd70cb81a7d9911c849
DeZerom <47084837+DeZerom@users.noreply.github.com>
2021-08-28 19:42:24 +0400

    TaskViewModel.completeTask implemented

--- app/src/main/java/com/example/taskmanager/viewmodels/TaskViewModel.kt
+++ app/src/main/java/com/example/taskmanager/viewmodels/TaskViewModel.kt
@@ -32,7 +32,11 @@ class TaskViewModel(application: Application): AndroidViewModel(application) {
     }
 
     fun completeTask(task: Task) {
-        deleteTask(task)
+        if (task.amount > 1) updateTask(
+            Task(task.id, task.name, task.projectOwnerId,
+            task.date, task.amount - 1)
+        )
+        else deleteTask(task)
     }
 
 }
